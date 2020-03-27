3fe11fd760c6366bb5b47594d2f924cb219cd202
Lucas Christopher <lucas.christopher.silva@ccc.ufcg.edu.br>
2020-03-27 16:44:09 -0300

    feat: back-end da aplicação

---
+++ backend/src/database/migrations/20200325115027_create_incidents.js
@@ -0,0 +1,22 @@
+// Method responsible for creating the table
+exports.up = function(knex) {
+  return knex.schema.createTable("incidents", function(table) {
+    table.increments();
+
+    table.string("title").notNullable();
+    table.string("description").notNullable();
+    table.decimal("value").notNullable();
+
+    table.string("ong_id").notNullable();
+
+    table
+      .foreign("ong_id")
+      .references("id")
+      .inTable("ongs");
+  });
+};
+
+// Redo the table, undoing the changes
+exports.down = function(knex) {
+  return knex.schema.dropTable("incidents");
+};
